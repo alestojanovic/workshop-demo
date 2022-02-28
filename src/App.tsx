c4e3c1b2e1df2f0c25c8dc94583da268923063f4
alestojanovic <alestojanovic@ebay.com>
2022-02-28 15:11:46 +0100

    feat: added button and hide/show logo functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const [toggle, setToggle] = useState(true);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {toggle ? <img src={logo} className="App-logo" alt="logo" /> : null}
+        <button className="App-button" onClick={() => setToggle(!toggle)}>
+          Click me
+        </button>
       </header>
     </div>
   );
