c948f6b026f1beaa70481399d2500184ffde2464
alestojanovic <alestojanovic@ebay.com>
2022-04-26 10:05:26 +0200

    feat: added toastify and useEffect demo

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,19 +1,26 @@
-import { fireEvent, render, screen } from "@testing-library/react";
-import App from "./App";
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable indent */
+/* eslint-disable no-undef */
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable semi */
+import { fireEvent, render, screen } from '@testing-library/react'
+import App from './App'
 
-test("Should show hide logo when button is toggled", () => {
-  // Renders the application
-  render(<App />);
+test('Should show hide logo when button is toggled', () => {
+    // Renders the application
+    render(<App />)
 
-  // Grabs logo element and checks if it is in the DOM
-  const logoImg = screen.queryByAltText("logo");
-  expect(logoImg).toBeInTheDocument();
+    // Grabs logo element and checks if it is in the DOM
+    const logoImg = screen.queryByAltText('logo')
+    expect(logoImg).toBeInTheDocument()
 
-  // Grabs button element, checks if it is in the DOM, and clicks on it
-  const toggleButton = screen.getByText("Click me");
-  expect(toggleButton).toBeInTheDocument();
-  fireEvent.click(toggleButton);
+    // Grabs button element, checks if it is in the DOM, and clicks on it
+    const toggleButton = screen.getByText('Click me')
+    expect(toggleButton).toBeInTheDocument()
+    fireEvent.click(toggleButton)
 
-  // Checks that logo element is NOT in the DOM after button is clicked
-  expect(logoImg).not.toBeInTheDocument();
-});
+    // Checks that logo element is NOT in the DOM after button is clicked
+    expect(logoImg).not.toBeInTheDocument()
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,37 @@
-import { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { useState, useEffect } from 'react'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
-  const [toggle, setToggle] = useState(true);
-  return (
-    <div className="App">
-      <header className="App-header">
-        {toggle ? <img src={logo} className="App-logo" alt="logo" /> : null}
-        <button className="App-button" onClick={() => setToggle(!toggle)}>
-          Click me
-        </button>
-      </header>
-    </div>
-  );
+    const [toggle, setToggle] = useState<boolean>(true)
+
+    useEffect(() => {
+        if (toggle === true) {
+            toast.success('Toggle on')
+        } else {
+            toast.warn('Toggle off')
+        }
+    })
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                {toggle ? (
+                    <img src={logo} className="App-logo" alt="logo" />
+                ) : null}
+                <button
+                    className="App-button"
+                    onClick={() => setToggle(!toggle)}
+                >
+                    Click me
+                </button>
+            </header>
+            <ToastContainer autoClose={3000} hideProgressBar />
+        </div>
+    )
 }
 
-export default App;
+export default App
